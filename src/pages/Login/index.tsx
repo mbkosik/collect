50e9079f1db19db71d84dd080fd8d9a9b85ba923
mbkosik <72941144+mbkosik@users.noreply.github.com>
2023-06-19 19:56:37 +0200

    Comment onError opt

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -34,9 +34,9 @@ export const Login = () => {
         },
       });
     },
-    onError: err => {
-      // TODO: display error notification;
-    },
+    // onError: err => {
+    //   // TODO: display error notification;
+    // },
   });
 
   const submitForm = form.onSubmit(values => handleLogin.mutate(values));
