3b1eb2a01e6b183d729d6e4f69aeca19a8ca2257
mbkosik <72941144+mbkosik@users.noreply.github.com>
2023-06-20 19:33:30 +0200

    Add logout logic

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -5,18 +5,35 @@ import {
   Title,
   Container,
   Text,
+  Button,
 } from '@mantine/core';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router';
+import { useAuth } from '@/store/auth-store';
 import type { ReactFCWithChildren } from '@/react.d.ts';
 import packages from '@/../package.json';
 
 export const AppLayout: ReactFCWithChildren = ({ children }) => {
   const { version } = packages;
+  const navigate = useNavigate();
+  const { dispatch } = useAuth();
+
+  const handleLogout = useCallback(() => {
+    navigate('/');
+    dispatch({
+      type: 'logout',
+    });
+  }, [navigate, dispatch]);
+
   return (
     <AppShell
       padding="md"
       navbar={
         <Navbar width={{ base: 300 }} height="100vh" p="xs">
           <Text>{version}</Text>
+          <Button variant="subtle" onClick={handleLogout}>
+            Logout
+          </Button>
         </Navbar>
       }
       header={
